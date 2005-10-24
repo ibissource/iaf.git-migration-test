c6b4c37e03a8f5377c533ccdeb9369e713c9111b
unknown2 <unknown2@ibissource.org>
2005-10-24 09:59:18 +0000

    Add support for pattern parameters, and include them into several listeners, senders and pipes that are file related

--- JavaSource/nl/nn/adapterframework/ftp/FtpListener.java
+++ JavaSource/nl/nn/adapterframework/ftp/FtpListener.java
@@ -1,35 +1,41 @@
 /*
- * $Log: FtpSender.java,v $
- * Revision 1.3  2005-10-24 09:59:22  europe\m00f531
+ * $Log: FtpListener.java,v $
+ * Revision 1.1  2005-10-24 09:59:19  europe\m00f531
  * Add support for pattern parameters, and include them into several listeners,
  * senders and pipes that are file related
  *
- * Revision 1.2  2005/10/17 12:21:21  unknown2
- * *** empty log message ***
- *
- * Revision 1.1  2005/10/11 13:03:29  unknown2
- * Supports retrieving files (FtpFileRetrieverPipe) and sending files (FtpSender)
- * via one of the FTP protocols (ftp, sftp, ftps both implicit as explicit).
+ * Revision 1.1  2005/10/11 13:00:21  unknown2
+ * New ibis file related elements, such as DirectoryListener, MoveFilePie and 
+ * BatchFileTransformerPipe
  *
  */
 package nl.nn.adapterframework.ftp;
 
-import nl.nn.adapterframework.configuration.ConfigurationException;
-import nl.nn.adapterframework.core.SenderException;
-import nl.nn.adapterframework.core.SenderWithParametersBase;
-import nl.nn.adapterframework.core.TimeOutException;
-import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import java.util.HashMap;
+import java.util.LinkedList;
 
-import org.apache.commons.lang.StringUtils;
+import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.INamedObject;
+import nl.nn.adapterframework.core.IPullingListener;
+import nl.nn.adapterframework.core.ListenerException;
+import nl.nn.adapterframework.core.PipeLineResult;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.log4j.Logger;
 
 /**
- * FTP client voor het versturen van files via FTP
+ * File {@link nl.nn.adapterframework.core.IPullingListener listener} that looks in a directory for files according to a wildcard. 
+ * When a file is found, it is moved to an outputdirectory, so that it isn't found more then once.  
+ * The name of the moved file is passed to the pipeline.  
  *
  * <p><b>Configuration:</b>
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
- * <tr><td>classname</td><td>nl.nn.ibis4fundation.FtpSender</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setName(String) name}</td><td>name of the sender</td><td>&nbsp;</td></tr>
+ * <tr><td>classname</td><td>nl.nn.ibis4fundation.DirectoryListener</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setName(String) name}</td><td>name of the listener</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setRemoteDirectory(String) inputDirectory}</td><td>Directory to look for files</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setResponseTime(long) responseTime}</td><td>Waittime to wait between polling</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setHost(String) host}</td><td>name or ip adres of remote host</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setPort(int) port}</td><td>portnumber of remote host</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setUsername(string) username}</td><td>name of the user to authenticatie on remote server</td><td>&nbsp;</td></tr>
@@ -45,63 +51,133 @@ import org.apache.commons.lang.StringUtils;
  * <tr><td>{@link #setTransferMode(string) mode}</td><td>transfermode, one of ASCII, BINARY, EBCDIC, STREAM, COMPRESSED</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setAllowSelfSignedCertificates(boolean) allowSelfSignedCertificates}</td><td>if true, the server certificate can be self signed</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setProtP(boolean) protP}</td><td>if true, the server returns data via another socket</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setMessageIsContent(boolean) messageIsContent}</td><td>if true, the contents of the message is send, otherwise it message contains the local filenames of the files to be send</td><td>&nbsp;</td></tr>
  * </table>
  * </p>
- *  
- * @author: John Dekker
+ *
+ * @version Id
+ * @author  john
  */
-public class FtpSender extends SenderWithParametersBase {
-	public static final String version = "$RCSfile: FtpSender.java,v $  $Revision: 1.3 $ $Date: 2005-10-24 09:59:22 $";
-	private String name;
-	private String remoteFilenamePattern;
-	private String remoteDirectory;
+public class FtpListener implements IPullingListener, INamedObject {
+	public static final String version = "$RCSfile: FtpListener.java,v $  $Revision: 1.1 $ $Date: 2005-10-24 09:59:19 $";
+
+	protected Logger log = Logger.getLogger(this.getClass());
 	private FtpSession ftpSession;
-	
-	public FtpSender() {
+	private String name;
+	private String remoteDirectory;
+	private long responseTime = 3600000; // one hour
+	private LinkedList remoteFilenames;
+
+	public FtpListener() {
 		this.ftpSession = new FtpSession();
 	}
 	
-	/* (non-Javadoc)
-	 * @see nl.nn.adapterframework.core.ISender#configure()
+
+	public void afterMessageProcessed(PipeLineResult processResult, Object rawMessage, HashMap context) throws ListenerException {
+	}
+
+	public void open() throws ListenerException {
+	}
+
+	public void close() throws ListenerException {
+	}
+
+	public HashMap openThread() throws ListenerException {
+		return null;
+	}
+
+	public void closeThread(HashMap threadContext) throws ListenerException {
+	}
+
+	/**
+	 * Configure does some basic checks (directoryProcessedFiles is a directory,  inputDirectory is a directory, wildcard is filled etc.);
+	 *
 	 */
 	public void configure() throws ConfigurationException {
 		ftpSession.configure();
-		
-		if (StringUtils.isEmpty(name)) {
-			throw new ConfigurationException("Attribute [name] is not set");
-		}
+		remoteFilenames = new LinkedList();
 	}
 
-	/* (non-Javadoc)
-	 * @see nl.nn.adapterframework.core.ISender#isSynchronous()
+	/**
+	 * Returns the name of the file in process (the {@link #archiveFile(File) archived} file) concatenated with the
+	 * record number. As te {@link #archiveFile(File) archivedFile} method always renames to a 
+	 * unique file, the combination of this filename and the recordnumber is unique, enabling tracing in case of errors
+	 * in the processing of the file.
+	 * Override this method for your specific needs! 
 	 */
-	public boolean isSynchronous() {
-		return true;
+	public String getIdFromRawMessage(Object rawMessage, HashMap threadContext) throws ListenerException {
+		String correlationId = rawMessage.toString();
+		threadContext.put("cid", correlationId);
+		return correlationId;
 	}
 
-	/* (non-Javadoc)
-	 * @see nl.nn.adapterframework.core.ISender#sendMessage(java.lang.String, java.lang.String)
+	/**
+	 * Retrieves a single record from a file. If the file is empty or fully processed, it looks wether there
+	 * is a new file to process and returns the first record.
 	 */
-	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException, TimeOutException {
-		try {
-			ftpSession.put(paramList, prc.getSession(), message, remoteDirectory, remoteFilenamePattern, true);
+	public synchronized Object getRawMessage(HashMap threadContext) throws ListenerException {
+		if (remoteFilenames.isEmpty()) {
+			try {
+				ftpSession.openClient(remoteDirectory);
+				remoteFilenames.addAll(ftpSession.ls(remoteDirectory, true));
+			}
+			catch(Exception e) {
+				throw new ListenerException(e); 
+			}
+			finally {
+				ftpSession.closeClient();
+			}
 		}
-		catch(SenderException e) {
-			throw e;
+		if (! remoteFilenames.isEmpty()) {
+			return remoteFilenames.removeFirst();
 		}
-		catch(Exception e) {
-			throw new SenderException("Error during ftp-ing " + message, e);
-		}
-		return message;
+		return waitAWhile();
 	}
 	
+	private Object waitAWhile() throws ListenerException {
+		try {
+			Thread.sleep(responseTime);
+			return null;
+		}
+		catch(InterruptedException e) {		
+			throw new ListenerException("Interrupted while listening", e);
+		}
+		
+	}
+
+	public String toString() {
+		String result = super.toString();
+		ToStringBuilder ts = new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE);
+		ts.append("name", getName());
+		ts.append("remoteDirectory", remoteDirectory);
+		result += ts.toString();
+		return result;
+
+	}
+	/**
+	 * Returns a string of the rawMessage
+	 */
+	public String getStringFromRawMessage(Object rawMessage, HashMap threadContext) throws ListenerException {
+		return rawMessage.toString();
+	}
+
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public long getResponseTime() {
+		return responseTime;
+	}
+	
+	public void setResponseTime(long responseTime) {
+		this.responseTime = responseTime;
+	}
+
+	public void setRemoteDirectory(String string) {
+		remoteDirectory = string;
 	}
 
 	public void setHost(String host) {
@@ -142,14 +218,6 @@ public class FtpSender extends SenderWithParametersBase {
 
 	public void setProxyUsername(String proxyUsername) {
 		ftpSession.setProxyUsername(proxyUsername);
-	}
-
-	public String getRemoteFilenamePattern() {
-		return remoteFilenamePattern;
-	}
-
-	public void setRemoteFilenamePattern(String string) {
-		remoteFilenamePattern = string;
 	}
 
 	public String getRemoteDirectory() {
@@ -199,9 +267,4 @@ public class FtpSender extends SenderWithParametersBase {
 	public void setProtP(boolean protP) {
 		ftpSession.setProtP(protP);
 	}
-
-	public void setMessageIsContent(boolean messageIsContent) {
-		ftpSession.setMessageIsContent(messageIsContent);
-	}
-
 }
